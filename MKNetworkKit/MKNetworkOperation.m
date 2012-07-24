d63bf1c2fb60d8cadf0cbaa26932dd1d22f38c30
Jordi Giménez <gimix@gimix.es>
2012-07-24 18:49:35 +0200

    adding PATCH method support

--- MKNetworkKit/MKNetworkOperation.m
+++ MKNetworkKit/MKNetworkOperation.m
@@ -639,7 +639,7 @@
   
   [displayString appendFormat:@" \"%@\"",  self.url];
   
-  if ([self.request.HTTPMethod isEqualToString:@"POST"] || [self.request.HTTPMethod isEqualToString:@"PUT"]) {
+  if ([self.request.HTTPMethod isEqualToString:@"POST"] || [self.request.HTTPMethod isEqualToString:@"PUT"] || [self.request.HTTPMethod isEqualToString:@"PATCH"]) {
     
     NSString *option = [self.filesToBePosted count] == 0 ? @"-d" : @"-F";
     if(self.postDataEncoding == MKNKPostDataEncodingTypeURL) {
@@ -823,7 +823,7 @@
   
   if(!self.isCancelled) {
     
-    if ([self.request.HTTPMethod isEqualToString:@"POST"] || [self.request.HTTPMethod isEqualToString:@"PUT"]) {            
+    if ([self.request.HTTPMethod isEqualToString:@"POST"] || [self.request.HTTPMethod isEqualToString:@"PUT"] || [self.request.HTTPMethod isEqualToString:@"PATCH"]) {            
       
       [self.request setHTTPBody:[self bodyData]];
     }
